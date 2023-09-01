31fc190e7d20f2c18e55608a681e9626b2af077d
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-01 12:17:19 -0600

    feat: a mocked storage and some testings.

--- intro-jest/src/__tests__/example_mocks.test.js
+++ intro-jest/src/__tests__/example_mocks.test.js
@@ -1,3 +1,7 @@
+import { storage } from '../lib/storage'
+import { getUsername, saveUsername } from '../user'
+jest.mock('../lib/storage')
+
 describe(' mocks', () => {
   test('first example', () => {
     const myMock = jest
@@ -16,5 +20,26 @@ describe(' mocks', () => {
     expect(result1).toBe(true)
     expect(result2).toBe('hello world')
     expect(result3).toBe(28)
+  })
+
+  test('second example', () => {
+    const userName = 'John Doe'
+    saveUsername(userName)
+    expect(storage.save).toHaveBeenCalledTimes(1)
+    expect(storage.save).toHaveBeenCalledWith({
+      key: 'username',
+      value: userName,
+    })
+  })
+
+  test('third example', () => {
+    const userName = 'John Doe'
+    storage.get.mockReturnValueOnce(userName)
+
+    const result = getUsername()
+
+    expect(result).toBe(userName)
+    expect(storage.get).toHaveBeenCalledTimes(1)
+    expect(storage.get).toHaveBeenCalledWith({ key: 'username' })
   })
 })
