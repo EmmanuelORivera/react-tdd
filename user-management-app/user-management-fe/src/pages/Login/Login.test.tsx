b5eea80f3fd64735d888a2f3db061c7f479a9699
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-04 14:59:18 -0600

    refactor: using a global to render component

--- user-management-app/user-management-fe/src/pages/Login/Login.test.tsx
+++ user-management-app/user-management-fe/src/pages/Login/Login.test.tsx
@@ -3,51 +3,41 @@ import Login from './Login'
 import userEvent from '@testing-library/user-event'
 
 describe('Login Component', () => {
-  it('should render the login title', () => {
+  beforeEach(() => {
     render(<Login />)
+  })
 
+  it('should render the login title', () => {
     expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument()
   })
 
   it('should render the form', () => {
-    render(<Login />)
-
     const form = screen.getByTestId('login-form')
     expect(form).toBeInTheDocument()
   })
   it('should render a label with the text of Email, with a control associated to that label', () => {
-    render(<Login />)
-
     const emailInput = screen.getByLabelText(/email/i)
     expect(emailInput).toBeInTheDocument()
     expect(emailInput).toHaveAttribute('type', 'email')
   })
 
   it('should render the email input as required', () => {
-    render(<Login />)
-
     const emailInput = screen.getByLabelText(/email/i)
     expect(emailInput).toHaveAttribute('required')
   })
 
   it('should render a label with the text of Password, with a control associated to that label', () => {
-    render(<Login />)
-
     const passwordInput = screen.getByLabelText(/password/i)
     expect(passwordInput).toBeInTheDocument()
     expect(passwordInput).toHaveAttribute('type', 'password')
   })
 
   it('should render the password input as required', () => {
-    render(<Login />)
-
     const passwordInput = screen.getByLabelText(/password/i)
     expect(passwordInput).toHaveAttribute('required')
   })
 
   it('should render a button with the Submit text', () => {
-    render(<Login />)
-
     const submitButton = screen.getByRole('button', { name: /submit/i })
 
     expect(submitButton).toBeInTheDocument()
@@ -55,8 +45,6 @@ describe('Login Component', () => {
   })
 
   it('should validate the inputs as required', async () => {
-    render(<Login />)
-
     userEvent.click(screen.getByRole('button', { name: /submit/i }))
 
     expect(
