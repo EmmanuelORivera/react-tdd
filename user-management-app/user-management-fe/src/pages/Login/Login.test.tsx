306e957c848bb5b764edff4d61a7ffb408e2d225
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-04 10:29:36 -0600

    refactor: wrapping tests with a describe method.

--- user-management-app/user-management-fe/src/pages/Login/Login.test.tsx
+++ user-management-app/user-management-fe/src/pages/Login/Login.test.tsx
@@ -1,33 +1,35 @@
 import { screen, render } from '@testing-library/react'
 import Login from './Login'
 
-it('should render the login title', () => {
-  render(<Login />)
+describe('Login Component', () => {
+  it('should render the login title', () => {
+    render(<Login />)
 
-  expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument()
-})
+    expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument()
+  })
 
-it('should render a label with the text of Email, with a control associated to that label', () => {
-  render(<Login />)
+  it('should render a label with the text of Email, with a control associated to that label', () => {
+    render(<Login />)
 
-  const emailInput = screen.getByLabelText(/email/i)
-  expect(emailInput).toBeInTheDocument()
-  expect(emailInput).toHaveAttribute('type', 'email')
-})
+    const emailInput = screen.getByLabelText(/email/i)
+    expect(emailInput).toBeInTheDocument()
+    expect(emailInput).toHaveAttribute('type', 'email')
+  })
 
-it('should render a label with the text of Password, with a control associated to that label', () => {
-  render(<Login />)
+  it('should render a label with the text of Password, with a control associated to that label', () => {
+    render(<Login />)
 
-  const passwordInput = screen.getByLabelText(/password/i)
-  expect(passwordInput).toBeInTheDocument()
-  expect(passwordInput).toHaveAttribute('type', 'password')
-})
+    const passwordInput = screen.getByLabelText(/password/i)
+    expect(passwordInput).toBeInTheDocument()
+    expect(passwordInput).toHaveAttribute('type', 'password')
+  })
 
-it('should render a button with the Submit text', () => {
-  render(<Login />)
+  it('should render a button with the Submit text', () => {
+    render(<Login />)
 
-  const passwordInput = screen.getByRole('button', { name: /submit/i })
+    const passwordInput = screen.getByRole('button', { name: /submit/i })
 
-  expect(passwordInput).toBeInTheDocument()
-  expect(passwordInput).toHaveAttribute('type', 'submit')
+    expect(passwordInput).toBeInTheDocument()
+    expect(passwordInput).toHaveAttribute('type', 'submit')
+  })
 })
