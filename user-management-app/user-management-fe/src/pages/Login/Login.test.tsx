713f59a649a9e0a85157b9b60fa022e36e83b672
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-04 10:47:05 -0600

    feat: making required inputs, also adding form tag

--- user-management-app/user-management-fe/src/pages/Login/Login.test.tsx
+++ user-management-app/user-management-fe/src/pages/Login/Login.test.tsx
@@ -8,6 +8,12 @@ describe('Login Component', () => {
     expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument()
   })
 
+  it('should render the form', () => {
+    render(<Login />)
+
+    const form = screen.getByTestId('login-form')
+    expect(form).toBeInTheDocument()
+  })
   it('should render a label with the text of Email, with a control associated to that label', () => {
     render(<Login />)
 
@@ -16,12 +22,26 @@ describe('Login Component', () => {
     expect(emailInput).toHaveAttribute('type', 'email')
   })
 
+  it('should render the email input as required', () => {
+    render(<Login />)
+
+    const emailInput = screen.getByLabelText(/email/i)
+    expect(emailInput).toHaveAttribute('required')
+  })
+
   it('should render a label with the text of Password, with a control associated to that label', () => {
     render(<Login />)
 
     const passwordInput = screen.getByLabelText(/password/i)
     expect(passwordInput).toBeInTheDocument()
     expect(passwordInput).toHaveAttribute('type', 'password')
+  })
+
+  it('should render the password input as required', () => {
+    render(<Login />)
+
+    const passwordInput = screen.getByLabelText(/password/i)
+    expect(passwordInput).toHaveAttribute('required')
   })
 
   it('should render a button with the Submit text', () => {
--- user-management-app/user-management-fe/src/pages/Login/Login.tsx
+++ user-management-app/user-management-fe/src/pages/Login/Login.tsx
@@ -4,13 +4,15 @@ const Login = () => {
   return (
     <>
       <h1 className="text-3xl">Login</h1>
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" />
+      <form data-testid="login-form">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" required />
 
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" name="password" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" required />
 
-      <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
