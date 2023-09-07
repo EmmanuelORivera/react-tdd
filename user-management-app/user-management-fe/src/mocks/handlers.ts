a6f811ef6ce01ada906b3db729068874460e1f8a
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-07 10:39:58 -0600

    feat: creation of a method to build a path depending on the base url.

--- user-management-app/user-management-fe/src/mocks/handlers.ts
+++ user-management-app/user-management-fe/src/mocks/handlers.ts
@@ -1,5 +1,13 @@
 import { rest } from 'msw'
 
+export const mswApi = (path: string) => {
+  return `${
+    process.env.REACT_APP_BASE_URL_BE || 'http://localhost:8080'
+  }${path}`
+}
+
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => res(ctx.delay(1), ctx.status(200))),
+  rest.post(mswApi('/login'), (req, res, ctx) =>
+    res(ctx.delay(1), ctx.status(200))
+  ),
 ]
