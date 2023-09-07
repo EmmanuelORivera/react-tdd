3a563f33102c31463f091acf37ecde37ebb30513
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-07 14:13:27 -0600

    feat(test): creation of Form Component and adding some tests

---
+++ store-app/src/components/Form/Form.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+import Form from './Form'
+
+describe('Form', () => {
+  beforeEach(() => {
+    render(<Form />)
+  })
+  it('there must be a create product form page', () => {
+    expect(
+      screen.queryByRole('heading', { name: /create product/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should contain the fields: name, size, type (electronic, furniture, clothing)', () => {
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
+  })
+})
---
+++ store-app/src/components/Form/Form.tsx
@@ -0,0 +1,31 @@
+import { TextField, NativeSelect, InputLabel } from '@mui/material'
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>
+      <h1>Create Product</h1>
+
+      <form>
+        <TextField label="name" id="name" />
+        <TextField label="size" id="size" />
+        <InputLabel variant="standard" htmlFor="type">
+          Type
+        </InputLabel>
+        <NativeSelect
+          defaultValue=""
+          inputProps={{
+            name: 'type',
+            id: 'type',
+          }}
+        >
+          <option value="electronic">Electronic</option>
+          <option value="furniture">Furniture</option>
+          <option value="clothing">Clothing</option>
+        </NativeSelect>
+      </form>
+    </>
+  )
+}
+
+export default Form
