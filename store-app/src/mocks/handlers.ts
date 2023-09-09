d349a1393e025a0e99df73d9cee9d05ab4ff9edc
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-09 13:33:33 -0600

    refactor: removing magic number

--- store-app/src/mocks/handlers.ts
+++ store-app/src/mocks/handlers.ts
@@ -1,9 +1,13 @@
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
+import { CREATED_STATUS } from '../consts/httpStatus'
 
 const server = setupServer(
   rest.post('/products', (req, res, ctx) => {
-    return res(ctx.status(201), ctx.json({ message: 'Mocked response' }))
+    return res(
+      ctx.status(CREATED_STATUS),
+      ctx.json({ message: 'Mocked response' })
+    )
   })
 )
 
